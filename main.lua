3f97f472e72602166a5ed9e615c38d1747bff2db
SodiumHub <feddyx780@gmail.com>
2026-09-16 21:15:22 +0200

    Initial upload

---
+++ main.lua
@@ -0,0 +1,5 @@
+-- Put your legit/normal script here.
+-- This file will be uploaded to GitHub initially when you run /postscript.
+-- When ScriptBlox approves the post, GitHub auto-switches to the MoonVeil loadstring.
+
+print('Script loaded!')
